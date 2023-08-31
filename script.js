0ca57c8a178a5773b6733a158e5cc4920420262c
MountainWing5796 <27727547+MountainWing5796@users.noreply.github.com>
2023-08-31 11:35:23 +0500

    changed dom elements selectors to data-attributes added functionality to show number buttons pressed on screen

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 // Calculator Functionality
 
-const simpleCalcRadio = document.querySelector('#simple');
-const scientificCalcRadio = document.querySelector('#scientific')
+const simpleCalcRadio = document.querySelector('[data-simple-radio]');
+const scientificCalcRadio = document.querySelector('[data-scientific-radio]');
 
-const angleUnits = document.querySelector('.angle-units')
-const scientificButtons = document.querySelector('.scientific')
+const angleUnits = document.querySelector('[data-angle-units]');
+const scientificButtons = document.querySelector('[data-scientific-buttons]');
+
+const inputScreen = document.querySelector('[data-input-screen]');
+const resultScreen = document.querySelector('[data-result-screen]');
+
+let firstNumber = '', secondNumber = '';
+function numberEnter(num) {
+    if (num == '0' && firstNumber === '0') {
+        firstNumber = '0';
+    } else if (num != '0' && firstNumber === '0') {
+        firstNumber = num;
+    } else {
+        firstNumber = firstNumber + num;
+    }
+    inputScreen.textContent = firstNumber;
+    console.log(typeof(num))
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+numberButtons.forEach((num)=> {
+    num.addEventListener('click', ()=> {
+        let numPress = num.dataset.number;
+        numberEnter(numPress);
+    })
+})
+
+
 
 simpleCalcRadio.addEventListener('click', ()=> {
     angleUnits.classList.add('displayNone')
@@ -15,3 +41,4 @@ scientificCalcRadio.addEventListener('click', ()=> {
     angleUnits.classList.remove('displayNone')
     scientificButtons.classList.remove('displayNone')
 })
+
